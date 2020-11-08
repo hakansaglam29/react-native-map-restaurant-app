1821a72f5309caf0e72f0cceacb00680cc62b6c6
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-11-09 00:41:01 +0100

    Restaurant detail added

--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
-import {restaurantDetailStyle} from '../Style';
+import { View, Text } from 'react-native';
+import Modal from 'react-native-modal';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { detail } from '../Style';
 
-const RestaurantDetail = () => {
-    return(
-        <SafeAreaView>
-            <View>
-                <Text> Restaurant Component</Text>
+const RestaurantDetail = (props) => {
+    return (
+        <Modal
+            isVisible={props.isVisible}
+            style={detail.modal}
+        >
+            <View style={detail.container}>
+                <Text style={detail.name}>{props.restaurant.name}</Text>
+                <Text style={detail.address}>
+                    {props.restaurant.area} {props.restaurant.address}
+                </Text>
+                <View
+                    style={detail.containertwo}>
+                    <View style={detail.icon}>
+                        <Icon name='phone' size={20} />
+                        <Text style={detail.phone}>+{props.restaurant.phone}</Text>
+                    </View>
+                    {/* <View style={detail.icon}>
+                        {showPrice(props.restaurant.price)}
+                    </View> */}
+                </View>
+                <Text>
+                    lpsum dolar sit amet, consectorem ietur adipiscing. Integer in lpsum dolar sit amet, consectorem ietur adipiscing. Integer in lpsum dolar sit amet, consectorem ietur adipiscing. Integer inlpsum dolar sit amet, consectorem ietur adipiscing. Integer in lpsum dolar sit amet, consectorem ietur adipiscing. Integer inlpsum dolar sit amet, consectorem ietur adipiscing. Integer inlpsum dolar sit amet, consectorem ietur adipiscing. Integer inlpsum dolar sit amet, consectorem ietur adipiscing. Integer in
+                    </Text>
             </View>
-        </SafeAreaView>
+        </Modal>
     )
 }
 
-export {RestaurantDetail};
+export { RestaurantDetail };
