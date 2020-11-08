1efaf9406ee51e7882c2e77e9dc334aa9c610a76
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-11-09 00:42:30 +0100

    format edit

--- src/components/City.js
+++ src/components/City.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {TouchableOpacity, View, Text} from 'react-native';
+import { TouchableOpacity, View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {cityStyle} from '../Style';
+import { cityStyle } from '../Style';
 
 const City = (props) => {
-    return(
-        <TouchableOpacity 
+    return (
+        <TouchableOpacity
             style={cityStyle.container}
             onPress={props.onSelect}
         >
-            <Icon name='city' size={20} color='#424242'/>
+            <Icon name='city' size={20} color='#424242' />
             <Text style={cityStyle.text}> {props.cityName}</Text>
         </TouchableOpacity>
     )
 }
 
-export {City};
+export { City };
