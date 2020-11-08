ed41d855860fb9183de4d8a7d416ba2c61dfa1d1
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-11-09 00:40:36 +0100

    format edit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import {View, TextInput} from 'react-native';
+import { View, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import {searchBarStyle} from '../Style'
+import { searchBarStyle } from '../Style'
 
 const SearchBar = (props) => {
-    return(
+    return (
         <View style={searchBarStyle.container}>
-            <Icon name='magnify' size={20} color='gray'/>
+            <Icon name='magnify' size={20} color='gray' />
             <TextInput
-            placeholder='Search a city'
-            style={searchBarStyle.input}
-            onChangeText={text => props.onSearch(text)}
-            
+                placeholder='Search a city'
+                style={searchBarStyle.input}
+                onChangeText={text => props.onSearch(text)}
+
             />
         </View>
     )
 }
 
-export {SearchBar};
+export { SearchBar };
