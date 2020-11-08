f89b57fdf6fbca5344faa28a4fb3e8f94c5e80fb
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-11-09 00:42:13 +0100

    restaurant detail added

--- src/Style.js
+++ src/Style.js
@@ -1,54 +1,74 @@
-import {StyleSheet, Dimensions} from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 export const searchBarStyle = StyleSheet.create({
-    container:{
-        backgroundColor:'white',
-        padding:5,
-        borderRadius:10,
+    container: {
+        backgroundColor: 'white',
+        padding: 5,
+        borderRadius: 10,
         width: Dimensions.get('window').width * 0.9,
-        top:5,
-        alignSelf:'center',
-        flexDirection:'row',
-        alignItems:'center',
+        top: 5,
+        alignSelf: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     input: {
-        padding:5
+        padding: 5
 
     }
 });
 
 
 export const cityStyle = StyleSheet.create({
-    container:{
-        padding:5,
-        margin:10,
-        borderRadius:10,
-        backgroundColor:'white',
-        borderWidth:1,
+    container: {
+        padding: 5,
+        margin: 10,
+        borderRadius: 10,
+        backgroundColor: 'white',
+        borderWidth: 1,
         borderColor: '#e0e0e0',
-        flexDirection:'row',
-        alignItems:'center',
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     text: {
-        fontWeight:'bold',
-        color:'#424242'
+        fontWeight: 'bold',
+        color: '#424242'
     }
 });
 
 
-export const RestaurantDetail = StyleSheet.create({
-    container:{
+export const detail = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
 
     },
+    containertwo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
     name: {
-
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#424242',
     },
 
     adress: {
-
+        fontSize: 12,
     },
 
     phone: {
-
-    }
+        fontWeight: 'bold',
+        marginLeft: 5,
+    },
+    icon: {
+        flexDirection: 'row',
+    },
+    modal: {
+        justifyContent: 'flex-end',
+        margin:0,
+        marginHorizontal: 10,
+    },
 });
