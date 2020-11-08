f78868ea0bf7f185c8f4dc6fb74a7c2e6b3e84f9
Hakan <67585490+hakansaglam29@users.noreply.github.com>
2020-11-09 00:41:55 +0100

    selected Restaurant added

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,8 @@
-import React, {useRef} from 'react';
-import {SafeAreaView, View, FlatList, Text} from 'react-native';
+import React, { useRef } from 'react';
+import { SafeAreaView, View, FlatList, Text } from 'react-native';
 import Axios from 'axios'
 
-import {City,RestaurantDetail,SearchBar} from "./components";
+import { City, RestaurantDetail, SearchBar } from "./components";
 import MapView from 'react-native-maps';
 
 
@@ -14,27 +14,29 @@ let originalList = [];
 export default Main = (props) => {
     const [cityList, setcityList] = React.useState([]);
     const [restaurants, setRestaurants] = React.useState([]);
+    const [visible, setVisible] = React.useState(false);
+    const [selectedRestaurant, setSelectedRestaurant] = React.useState([]);
     const mapRef = useRef([]);
 
 
     // Burada {data} yerine response yazabilirdik. Response.data ile de cagirirdik.
     const fetchCities = async () => {
-        const {data} = await Axios.get(
+        const { data } = await Axios.get(
             "http://opentable.herokuapp.com/api/cities",
-            );
-            setcityList(data.cities);
-            originalList = [...data.cities]
+        );
+        setcityList(data.cities);
+        originalList = [...data.cities]
     }
 
     React.useEffect(() => {
-            fetchCities();
-        }, [])
+        fetchCities();
+    }, [])
 
-    const renderItem = ({item}) => <City cityName={item} onSelect={() => onCitySelect(item)}/>;
+    const renderItem = ({ item }) => <City cityName={item} onSelect={() => onCitySelect(item)} />;
 
 
 
-    function onCitySearch(text){
+    function onCitySearch(text) {
         const filteredCityList = originalList.filter(item => {
             const userText = text.toUpperCase();
             const cityName = item.toUpperCase();
@@ -44,58 +46,70 @@ export default Main = (props) => {
     }
 
     const onCitySelect = async (city) => {
-    
-        const {data: {restaurants: myRes}} = await Axios.get("http://opentable.herokuapp.com/api/restaurants?city=" + city,);
-    
+
+        const { data: { restaurants: myRes } } = await Axios.get("http://opentable.herokuapp.com/api/restaurants?city=" + city);
+
         const restaurantLatLong = myRes.map(res => {
-        return {
+            return {
                 latitude: res.lat,
                 longitude: res.lng,
-                    }
+            }
         })
         mapRef.current.fitToCoordinates(restaurantLatLong, {
-            edgePadding:{top: 150,
-            right: 100,
-            bottom: 100,
-            left: 100}
-          })
+            edgePadding: {
+                top: 150,
+                right: 100,
+                bottom: 100,
+                left: 100
+            }
+        })
 
         setRestaurants(myRes)
         console.log(restaurantLatLong)
     }
 
+    function onRestaurantSelect(marker) {
+        setSelectedRestaurant(marker);
+        setVisible(true)
+    }
 
-    return(
-        <SafeAreaView style={{flex:1}}>
-            <View style={{flex:1}}>
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
                 <MapView
-                ref={mapRef}
-                style={{flex:1}}
-                initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+                    ref={mapRef}
+                    style={{ flex: 1 }}
+                    initialRegion={{
+                        latitude: 41.03825,
+                        longitude: -95.9624,
+                        latitudeDelta: 29.3922,
+                        longitudeDelta: 57.4421,
                     }}>
 
-                {restaurants.map((marker, index) => (
-                    <MapView.Marker
-                        key={index}
-                        coordinate={{
-                            latitude: marker.lat,
-                            longitude: marker.lng,
-                        }}
-                    />))}
+                    {restaurants.map((marker, index) => (
+                        <MapView.Marker
+                            key={index}
+                            coordinate={{
+                                latitude: marker.lat,
+                                longitude: marker.lng,
+                            }}
+                            onPress={() => onRestaurantSelect(marker)}
+                        />))}
 
                 </MapView>
-                <View style={{position:'absolute'}}>
-                    <SearchBar onSearch={onCitySearch}/>
+                <View style={{ position: 'absolute' }}>
+                    <SearchBar onSearch={onCitySearch} />
                     <FlatList
                         horizontal
-                        showsHorizontalScrollIndicator = {false}
+                        showsHorizontalScrollIndicator={false}
                         data={cityList}
                         renderItem={renderItem}
                         keyExtractor={(_, index) => index.toString()}
+                    />
+                    <RestaurantDetail
+                        isVisible={visible}
+                        restaurant={selectedRestaurant}
                     />
                 </View>
             </View>
